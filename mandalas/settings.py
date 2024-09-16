9fe6b1b1af6bce2f86d31d45c5410f3cf1bda646
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-09-16 10:53:14 +0000

    Add secret key

--- mandalas/settings.py
+++ mandalas/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)w)vhis&%^6=^=x5kr3)4866=w&8^i98y(9lg7!=uoz-n986k4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
