59d4125c748057f75365b4e01028c3d5c26b1435
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-09-23 10:21:15 +0000

    Add contact app and CRUD for comments

--- mandalas/settings.py
+++ mandalas/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-meghanarajv-artofmandal-vrrqgkk64y2.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog',
     'about',
+    'contact',
 ]
 
 SITE_ID = 1
